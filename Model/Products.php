613a70909830b7eae72cc156948c6f264ad8c2f2
DenGian <mondelaers.ian@gmail.com>
2022-07-08 16:39:30 +0200

    add products class

--- Model/Products.php
+++ Model/Products.php
@@ -1,6 +1,31 @@
 <?php
+declare(strict_types=1);
 
 class Products
 {
+    private int $id;
+    private string $name;
+    private int $price;
 
+    public function __construct(string $name, int $price)
+    {
+        //$this->id= $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getProductId(): int
+    {
+        return $this->id;
+    }
+
+    public function getProductName(): string
+    {
+        return $this->name;
+    }
+
+    public function getProductPrice(): int
+    {
+        return $this->price;
+    }
 }
