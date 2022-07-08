5385a1e3997666583c518309707ee7859c9f24a7
DenGian <mondelaers.ian@gmail.com>
2022-07-08 10:55:57 +0200

    added dropdown boilerplate

--- View/homepage.php
+++ View/homepage.php
@@ -9,3 +9,13 @@ Anything complex should be calculated in the model -->
     <p>Put your content here.</p>
 </section>-->
 <?php require 'includes/footer.php'?>
+<fieldset>
+    <label for="product">Choose a product:</label>
+    <select name="product" id="product"><!--select is used to create a drop-down list-->
+            <option value=""></option>
+    </select>
+    <label for="customers">Choose a customer:</label>
+    <select name="customers" id="customers">
+        <option value=""></option>
+    </select>
+</fieldset>
