69474e06e05bb2a264f87905fbce0bf77f32bd80
Wendy Quarteer <wendyquarteer@outlook.com>
2022-07-09 09:34:28 +0200

    added drop-down for quantity

--- View/homepage.php
+++ View/homepage.php
@@ -8,36 +8,56 @@ Anything complex should be calculated in the model -->
 
     <p>Please, select a product & customer to know the price.</p> <!-- ADDED WEEKEND -->
 </section>
-<form method = "post"> <!-- ADDED WEEKEND -->
-<fieldset>
-    <legend>Look-up</legend> <!-- ADDED WEEKEND -->
-    <label for="product">Choose a product:</label>
-    <select name="product" id="product"><!--select is used to create a drop-down list-->
-        <?php foreach ($products as $product) {
-            echo "<option>$product<option>";
-        } ?>
-    </select>
+<form method="post"> <!-- ADDED WEEKEND -->
+    <fieldset>
+        <legend>Look-up</legend> <!-- ADDED WEEKEND -->
 
-<!-- TODO: switch places -->
+        <label for="customers">Choose a customer:</label>
+        <select name="customers" id="customers">
+            <?php foreach ($customers as $key => $customer) {//loop through the customers array.
+                echo "<option>$key - $customer<option>"; //display lastname - Firstname in drop-down-list.
+            } ?>
+        </select>
 
-    <label for="customers">Choose a customer:</label>
-    <select name="customers" id="customers">
-        <?php foreach ($customers as $key => $customer) {//loop through the customers array.
-            echo "<option>$key - $customer<option>"; //display lastname - Firstname in drop-down-list.
-        } ?>
-    </select>
-</fieldset>
-    <button type="submit"  name="submit" autofocus><span style="cursor: pointer">SUBMIT</span></button><!-- ADDED WEEKEND -->
+        <label for="product">Choose a product:</label>
+        <select name="product" id="product"><!--select is used to create a drop-down list-->
+            <?php foreach ($products as $product) {
+                echo "<option>$product<option>";
+            } ?>
+        </select>
+
+        <label for="pieces">Pieces: </label><!-- ADDED WEEKEND -->
+        <select name="pieces" id="pieces"><!-- ADDED WEEKEND -->
+            <option>1<option><!-- ADDED WEEKEND -->
+            <option>2<option>
+            <option>3<option>
+            <option>4<option>
+            <option>5<option>
+            <option>6<option>
+            <option>7<option>
+            <option>8<option>
+            <option>9<option>
+            <option>10<option>
+            <option>11<option>
+            <option>12<option>
+            <option>13<option>
+            <option>14<option>
+            <option>15<option>
+        </select><!-- ADDED WEEKEND -->
+
+    </fieldset>
+
+    <button type="submit" name="submit" autofocus><span style="cursor: pointer">SUBMIT</span></button><!-- ADDED WEEKEND -->
 </form><!-- ADDED WEEKEND -->
 
 
 <fieldset><!-- ADDED WEEKEND -->
     <legend>Result</legend><!-- ADDED WEEKEND -->
-<label for="piece">Normal price per piece:</label><!-- ADDED WEEKEND -->
+    <label for="piece">Normal price per piece:</label><!-- ADDED WEEKEND -->
     <label for="total">Normal total price:</label><!-- ADDED WEEKEND -->
 
 </fieldset><!-- ADDED WEEKEND -->
-<?php require 'includes/footer.php'?>
+<?php require 'includes/footer.php' ?>
 
 
 
