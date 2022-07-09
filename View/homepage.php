fc3718d64594b2f8cd94b2874e71a1d5e7507331
Wendy Quarteer <wendyquarteer@outlook.com>
2022-07-09 09:23:37 +0200

    added field for reslut

--- View/homepage.php
+++ View/homepage.php
@@ -1,21 +1,24 @@
 <?php require 'includes/header.php' ?>
 <!-- this is the view, try to put only simple if's and loops here.
 Anything complex should be calculated in the model -->
-<!--<section>
-    <h4>Hello <?php /*echo $user->getName()*/ ?>,</h4>
+<section>
+    <!--<h4>Hello <?php /*echo $user->getName()*/ ?>,</h4>-->
 
-    <p><a href="index.php?page=info">To info page</a></p>
+    <!--<p><a href="index.php?page=info">To info page</a></p>-->
 
-    <p>Put your content here.</p>
-</section>-->
+    <p>Please, select a product & customer to know the price.</p> <!-- ADDED WEEKEND -->
+</section>
 <form method = "post"> <!-- ADDED WEEKEND -->
 <fieldset>
+    <legend>Look-up</legend> <!-- ADDED WEEKEND -->
     <label for="product">Choose a product:</label>
     <select name="product" id="product"><!--select is used to create a drop-down list-->
         <?php foreach ($products as $product) {
             echo "<option>$product<option>";
         } ?>
     </select>
+
+<!-- TODO: switch places -->
 
     <label for="customers">Choose a customer:</label>
     <select name="customers" id="customers">
@@ -26,6 +29,14 @@ Anything complex should be calculated in the model -->
 </fieldset>
     <button type="submit"  name="submit" autofocus><span style="cursor: pointer">SUBMIT</span></button><!-- ADDED WEEKEND -->
 </form><!-- ADDED WEEKEND -->
+
+
+<fieldset><!-- ADDED WEEKEND -->
+    <legend>Result</legend><!-- ADDED WEEKEND -->
+<label for="piece">Normal price per piece:</label><!-- ADDED WEEKEND -->
+    <label for="total">Normal total price:</label><!-- ADDED WEEKEND -->
+
+</fieldset><!-- ADDED WEEKEND -->
 <?php require 'includes/footer.php'?>
 
 
