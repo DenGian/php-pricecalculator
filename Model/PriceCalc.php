5d163dc084a431424362535a572fde142a2b3314
DenGian <mondelaers.ian@gmail.com>
2022-07-11 15:33:25 +0200

    add coments for clearity

--- Model/PriceCalc.php
+++ Model/PriceCalc.php
@@ -22,23 +22,23 @@ class PriceCalc
      * @param int $fixed_discount
      * @param int $variable_discount
      */
-    public function __construct(Database $connection, Client $customer, Products $product, int $pieces, int $price, int $fixed_discount, int $variable_discount)
-    {
-        $this->connection = $connection;
-        $this->customer = $customer;
-        $this->product = $product;
-        $this->pieces = $pieces;
-        $this->price = $price;
-        $this->fixed_discount = $fixed_discount;
-        $this->variable_discount = $variable_discount;
-    }
+//    public function __construct(Database $connection, Client $customer, Products $product, int $pieces, int $price, int $fixed_discount, int $variable_discount)
+//    {
+//        $this->connection = $connection;
+//        $this->customer = $customer;
+//        $this->product = $product;
+//        $this->pieces = $pieces;
+//        $this->price = $price;
+//        $this->fixed_discount = $fixed_discount;
+//        $this->variable_discount = $variable_discount;
+//    }
 
-    public function productFetch($POST)
-    {
-        $price = $this->connect()->query("SELECT price, FROM product WHERE name =" . $POST['product']);
-        $fetchPrice = $price->fetch();
-        return $fetchPrice;
-    }
+//    public function productFetch($POST)
+//    {
+//        $price = $this->connect()->query("SELECT price, FROM product WHERE name =" . $POST['product']);
+//        $fetchPrice = $price->fetch();
+//        return $fetchPrice;
+//    }
 
 
 
